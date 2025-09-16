11d9b6df8549190b6d51d48ed721e06b24ff1790
Lhuana <lhuanaclopez14@gmail.com>
2025-09-15 23:00:29 -0500

    Agregar componente App con estructura básica y componentes Navbar, HeroSection y About

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <HeroSection />
+      <About />
+    </>
+  );
+}
+
+export default App;
